c1072a2722c8f1bc5aded5696a896fcfb99c334a
Mario Leiva <mleiva.robles@gmail>
2025-11-24 14:51:56 -0300

    Conjetura de collatz

---
+++ collatz.c
@@ -0,0 +1,40 @@
+/*
+Conjetura de Collatz
+Sea la siguiente operación, aplicable a cualquier número entero positivo:
+Si el número es par, se divide entre 2.
+Si el número es impar, se multiplica por 3 y se suma 1.
+
+gcc -o collatz collatz.c
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+    long long int n;
+    int numeros[2] = {0};
+
+    printf("Ingresa un numero entero positivo n>0: ");
+    scanf(" %lld", &n);
+
+    if (n <= 0 ){
+        printf("Entrada invalida. Debes introducir un entero positivo.\n");
+        return 1;
+    }
+    
+    while (n != 1){
+        printf("%lld ", n);
+
+        if( n % 2 == 0){
+            n = n / 2;
+            numeros[0]++;
+        } else {
+            n = 3 * n + 1;
+            numeros[1]++;
+        }
+    }
+    printf("1\n");
+    printf("pares: %d\n", numeros[0]);
+    printf("impares: %d\n", numeros[1]+1);
+}
